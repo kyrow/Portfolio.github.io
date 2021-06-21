e185ffc1d988204a59d052ec1d4a010ce0fd7810
Ampersandr <mr.khan4ik@gmail.com>
2021-06-21 19:30:17 +0600

    pubticPath2

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env, argv) => {
 	const isProductionBuild = argv.mode === "production";
-	const publicPath = "Portfolio.github.io";
+	const publicPath = "/dist";
 
 	const pcss = {
 		test: /\.(p|post|)css$/,
