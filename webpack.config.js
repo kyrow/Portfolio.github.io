fef4e518187946670ad9c9023a227c09e3b4c3ef
Ampersandr <mr.khan4ik@gmail.com>
2021-06-22 21:23:37 +0600

    dist-gh

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env, argv) => {
 	const isProductionBuild = argv.mode === "production";
-	const publicPath = "D:/Projects/Portfolio/vue-boilerplate/dist";
+	const publicPath = "/dist/";
 
 	const pcss = {
 		test: /\.(p|post|)css$/,
