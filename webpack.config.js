9f67a9b09913c2862f82bae53deae7928a071347
Ampersandr <mr.khan4ik@gmail.com>
2021-06-23 19:35:38 +0600

    ppgh

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env, argv) => {
 	const isProductionBuild = argv.mode === "production";
-	const publicPath = "https://kyrow.github.io/Portfolio.github.io/";
+	const publicPath = "https://kyrow.github.io/Portfolio.github.io/dist/";
 
 	const pcss = {
 		test: /\.(p|post|)css$/,
